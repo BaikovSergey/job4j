d66489cda5e7f70e5e08a43ea92cbc3960ce66ad
Sergey Baikov <rayleonard9@gmail.com>
2018-10-28 09:41:55 +0300

    6.4. Слово начинается с ...[#76390]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Sergey Baikov
+ * @version $Id$
+ * @since 28.10.18
+ */
+public class ArrayCharTest {
+
+    /**
+     * Test startWith.
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Test startWith.
+     */
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
