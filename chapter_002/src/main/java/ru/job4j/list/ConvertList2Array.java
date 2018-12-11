2f952c07b36ac3a1d7eb17e6386d9dbe9f309e55
Sergey Baikov <rayleonard9@gmail.com>
2018-12-11 17:16:50 +0300

    3. Конвертация ArrayList в двухмерный массив

---
+++ chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,34 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 11.12.18
+ */
+public class ConvertList2Array {
+
+    /**
+     * Метод конвертирует список в двумерный массив
+     * @param list список
+     * @param rows количество строк массива
+     * @return двумерный массив
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int[][] array = new int[rows][rows];
+        int length = list.size();
+        int listIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array.length; j++) {
+                    array[i][j] = list.get(listIndex);
+                    listIndex++;
+                    length--;
+                    if (length == 0){
+                        break;
+                    }
+                }
+            }
+        return array;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 11.12.18
+ */
+public class ConvertList2ArrayTest {
+
+    /**
+     * Test ConvertList2Array
+     */
+    @Test
+    public void when7ElementsThen9() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                3
+        );
+        int[][] expect = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0 ,0}
+        };
+        assertThat(result, is(expect));
+    }
+}
