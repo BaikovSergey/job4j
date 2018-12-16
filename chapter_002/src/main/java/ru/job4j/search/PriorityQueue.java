513f77e6fe7a908e355ae2b6f539d68e39a5cc6d
Sergey Baikov <rayleonard9@gmail.com>
2018-12-16 13:27:01 +0300

    2. Очередь с приоритетом на LinkedList

--- chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -25,8 +25,27 @@ public class PriorityQueue {
         }
     }
 
-    public Task take() {
+    /**
+     * Метод для получения первого элемента в списке.
+     * @return первый элемент.
+     */
+    public Task takeFirst() {
         return this.tasks.get(0);
     }
 
+    /**
+     * Метод для получения второго элемента в списке.
+     * @return второй элемент.
+     */
+    public Task takeMid() {
+        return this.tasks.get(1);
+    }
+
+    /**
+     * Метод для получения третьего элемента в списке.
+     * @return третий элемент.
+     */
+    public Task takeLast() {
+        return this.tasks.get(2);
+    }
 }
--- chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -4,14 +4,27 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 16.12.18
+ */
 public class PriorityQueueTest {
+
+    /**
+     * Test put.
+     */
     @Test
     public void whenHigherPriority() {
         PriorityQueue queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
-        assertThat(result.getDesc(), is("urgent"));
+        Task first = queue.takeFirst();
+        Task mid = queue.takeMid();
+        Task last = queue.takeLast();
+        assertThat(first.getDesc(), is("urgent"));
+        assertThat(mid.getDesc(), is("middle"));
+        assertThat(last.getDesc(), is("low"));
     }
 }
