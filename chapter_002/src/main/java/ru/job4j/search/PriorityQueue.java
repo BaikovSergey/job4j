d7c6024ee0263ebc9164459d2acd9b232234471e
Sergey Baikov <rayleonard9@gmail.com>
2018-12-17 17:10:55 +0300

    список с приоритетами

--- chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,21 +5,23 @@ import java.util.LinkedList;
 public class PriorityQueue {
 
     private LinkedList<Task> tasks = new LinkedList<>();
+
     /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определять по полю приоритет.
      * Для вставик использовать add(int index, E value)
      * @param task задача
      */
-
     public void put(Task task) {
         if (tasks.isEmpty()) {
             tasks.add(task);
+        } else if (task.getPriority() > tasks.getLast().getPriority()) {
+            tasks.addLast(task);
         } else {
-            for (int i = 0; i < tasks.size(); i++) {
-                if (task.getPriority() < tasks.get(i).getPriority()) {
-                    tasks.add(i, task);
-                    break;
+        for (int i = 0; i < tasks.size(); i++) {
+            if (task.getPriority() < tasks.get(i).getPriority()) {
+                tasks.add(i, task);
+                break;
                 }
             }
         }
--- chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_002/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -20,7 +20,14 @@ public class PriorityQueueTest {
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task first = queue.take();
-        assertThat(first.getDesc(), is("urgent"));
+        queue.put(new Task("last", 8));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+        result = queue.take();
+        assertThat(result.getDesc(), is("middle"));
+        result = queue.take();
+        assertThat(result.getDesc(), is("low"));
+        result = queue.take();
+        assertThat(result.getDesc(), is("last"));
     }
 }
