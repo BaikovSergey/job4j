c748a330e1588878414f357384f61797b39e5b42
Sergey Baikov <rayleonard9@gmail.com>
2019-01-09 16:56:13 +0300

    3. Компаратор для строк.

---
+++ chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,39 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 09.01.19
+ */
+public class ListCompare implements Comparator<String> {
+
+    /**
+     * Компаратор для сравнения двух массивов символов
+     * @param left первая строка
+     * @param right вторая строка
+     * @return 0, если строки одинаковые. 1, если первая строка длинее второй или буква в первом слове больше чем во втором.
+     * -1, если первая строка короче второй или буква в первом слове меньше чем во втором.
+     */
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        if ((left != null && right != null && !left.equals(right))) {
+            if (left.length() > right.length()) {
+                result = 1;
+            } else if (left.length() == right.length()) {
+               for (int i = 0; i < left.length(); i++) {
+                   if (left.charAt(i) > right.charAt(i)) {
+                       result = 1;
+                   } else if (left.charAt(i) < right.charAt(i)) {
+                       result = -1;
+                   }
+               }
+            } else {
+                result = -1;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/comparator/ListCompareTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.comparator;
+
+import org.junit.Test;
+
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 09.01.19
+ */
+public class ListCompareTest {
+
+    /**
+     * Test compare
+     */
+    @Test
+    public void whenStringsAreEqualThenZero () {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    /**
+     * Test compare
+     */
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative () {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    /**
+     * Test compare
+     */
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive () {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Petrova",
+                "Ivanov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    /**
+     * Test compare
+     */
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive(){
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    /**
+     * Test compare
+     */
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative(){
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Patrov",
+                "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+}
