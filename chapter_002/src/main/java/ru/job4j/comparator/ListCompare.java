4b07e3e6fe179fb84eced210e06d8762f045c390
Sergey Baikov <rayleonard9@gmail.com>
2019-01-10 12:40:49 +0300

    3. Компаратор для строк.

--- chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -11,27 +11,32 @@ public class ListCompare implements Comparator<String> {
 
     /**
      * Компаратор для сравнения двух массивов символов.
-     * @param left первая строка
+     *
+     * @param left  первая строка
      * @param right вторая строка
      * @return 0, если строки одинаковые. 1, если первая строка длинее второй или буква в первом слове больше чем во втором.
      * -1, если первая строка короче второй или буква в первом слове меньше чем во втором.
      */
+
+
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        if ((left != null && right != null && !left.equals(right))) {
-            if (left.length() > right.length()) {
-                result = 1;
-            } else if (left.length() == right.length()) {
-               for (int i = 0; i < left.length(); i++) {
-                   if (left.charAt(i) > right.charAt(i)) {
-                       result = 1;
-                   } else if (left.charAt(i) < right.charAt(i)) {
-                       result = -1;
-                   }
-               }
+        if (left != null && right != null && !left.equals(right)) {
+            if (left.length() != right.length()) {
+                if (left.length() > right.length()) {
+                    result = 1;
+                } else {
+                    result = -1;
+                }
             } else {
-                result = -1;
+                for (int i = 0; i < left.length(); i++) {
+                    if (left.charAt(i) > right.charAt(i)) {
+                        result = 1;
+                    } else if (left.charAt(i) < right.charAt(i)) {
+                        result = -1;
+                    }
+                }
             }
         }
         return result;
