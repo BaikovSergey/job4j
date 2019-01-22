8a47033e5dd83b9d3ea827f12d3423a722523dcf
Sergey Baikov <rayleonard9@gmail.com>
2019-01-22 12:51:25 +0300

    Компаратор для строк

--- chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -17,12 +17,10 @@ public class ListCompare implements Comparator<String> {
      * @return 0, если строки одинаковые. 1, если первая строка длинее второй или буква в первом слове больше чем во втором.
      * -1, если первая строка короче второй или буква в первом слове меньше чем во втором.
      */
-
-
     @Override
     public int compare(String left, String right) {
         int result = Integer.compare(left.length(), right.length());
-        int lenght = Math.min(left.length(), right. length());
+        int lenght = Math.min(left.length(), right.length());
         for (int i = 0; i < lenght; i++) {
             int a = Character.compare(left.charAt(i), right.charAt(i));
             if (a != 0) {
