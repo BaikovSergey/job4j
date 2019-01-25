173505ce574ce330c2f4755043f2a1e6e5b9776f
Sergey Baikov <rayleonard9@gmail.com>
2019-01-25 11:58:04 +0300

    Подсчет функции в диапазоне.

---
+++ chapter_002/src/main/java/ru/job4j/lambda/Diapason.java
@@ -0,0 +1,28 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 24.01.19
+ */
+public class Diapason {
+
+    /**
+     * Метод расчитыает значение заданной функции в диапазоне.
+     * @param start начальное значение.
+     * @param end конечное значение.
+     * @param func функция.
+     * @return результат в виде списка.
+     */
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> result = new ArrayList<>();
+        for (int index = start; index !=end; index++) {
+            result.add(func.apply((double) index));
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Sergey Baikov
+ * @version $ 1 $
+ * @since 24.01.19
+ */
+public class DiapasonTest {
+
+    /**
+     * Test linear function.
+     */
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test quadratic function function.
+     */
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(x, 2) + 1);
+        List<Double> expected = Arrays.asList(26D, 37D, 50D);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test quadratic function function.
+     */
+    @Test
+    public void whenLogarithmFunctionThenLogarithmResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(1, 3, x -> Math.log(x) / Math.log(2)+ 1);
+        List<Double> expected = Arrays.asList(1D, 2D);
+        assertThat(result, is(expected));
+    }
+}
